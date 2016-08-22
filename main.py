68cada2d4ae8e1595393a30ba443a1ce6c2df6d3
knuckledragger314 <charlie.edelen.4@gmail.com>
2016-08-22 14:51:28 -0500

    pretty much done with part 1a and 1b

--- main.py
+++ main.py
@@ -15,10 +15,84 @@
 # limitations under the License.
 #
 import webapp2
+from caesar import encrypt
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ROT 13 Page</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <a href="/">Rot Thirteen</a>
+    </h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        edit_header = "<h3>ROT 13 Practice</h3>"
+
+        # a field for adding a phrae to encrypt
+        first_form = """
+        <form method="post">
+            <label>
+                How many characters to rotate by?
+                <p></p>
+                    <input type="text" name="rotate-by"/>
+            </label>
+            <label>
+                <textarea name="text" rows="4" cols="40"></textarea>
+            </label>
+            <input type="submit">
+        </form>
+        """
+
+
+        main_content = edit_header + first_form
+        user_response = page_header + main_content + page_footer
+        self.response.write(user_response)
+
+    def post(self):
+        edit_header = "<h3>ROT 13 Practice</h3>"
+        text_answer = self.request.get("text")
+        rotate_by_str = self.request.get("rotate-by")
+        rotate_by = int(rotate_by_str)
+        rot_text = encrypt(text_answer, rotate_by)
+        # a field for adding a phrae to encrypt
+        encrypt_form = """
+        <form method="post">
+            <label>
+                How many characters to rotate by?
+                <p></p>
+                    <input type="text" name="rotate-by"/>
+            </label>
+            <p></p>
+            <label>
+                <textarea name="text" rows="4" cols="40">%s</textarea>
+            </label>
+            <input type="submit">
+        </form>
+        """ % (rot_text)
+        main_content = edit_header + encrypt_form
+        user_response = page_header + main_content + page_footer
+        #text_answer = self.request.get("text")
+        #rot_text = encrypt(text_answer, 13)
+        self.response.write(user_response)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
